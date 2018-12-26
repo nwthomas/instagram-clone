80a9a7c34c9cc643bda0140a9d4ea632ef6f3b02
Nathan Thomas <nwthomas@me.com>
2018-12-25 22:43:32 -0800

    Complete post header and main img component sections

--- instagram/src/Components/PostContainer/Post.js
+++ instagram/src/Components/PostContainer/Post.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./PostContainer.css";
 
 const Post = props => {
   return (
     <React.Fragment>
       <div className="post__icon">
-        <img src={props.thumbnailUrl} alt="" />
+        <div className="thumbnail">
+          <img src={props.thumbnailUrl} alt="" />
+        </div>
         <a href="index.html">{props.username}</a>
       </div>
       <img src={props.imageUrl} alt="" />
