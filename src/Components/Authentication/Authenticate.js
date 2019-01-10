c1f58015abd3fc4677d86d2ec5c44fc8fc146a40
Nathan Thomas <nwthomas@me.com>
2019-01-10 11:23:27 -0800

    Implement login btn color change on username input

--- src/Components/Authentication/Authenticate.js
+++ src/Components/Authentication/Authenticate.js
@@ -30,11 +30,21 @@ const Authenticate = App => {
       localStorage.setItem("fullName", this.state.fullName);
     };
 
+    loginBtnChange = () => {
+      const loginButton = document.querySelector(".login__button");
+      this.state.username
+        ? loginButton.classList.add("login__button--active")
+        : loginButton.classList.remove("login__button--active");
+    };
+
     onChange = event => {
       if (event.target.name === "login-username") {
-        this.setState({
-          username: event.target.value
-        });
+        this.setState(
+          {
+            username: event.target.value
+          },
+          () => this.loginBtnChange()
+        );
       } else if (event.target.name === "login-password") {
         this.setState({
           password: event.target.value
